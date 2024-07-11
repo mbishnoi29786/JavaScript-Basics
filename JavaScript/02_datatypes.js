445ea36917cafbdb08bc9081040ef21a29216e84
mbishnoi29786 <122912118+mbishnoi29786@users.noreply.github.com>
2024-07-11 13:39:18 +0000

    Stack and heap in Data Types

--- JavaScript/02_datatypes.js
+++ JavaScript/02_datatypes.js
@@ -16,3 +16,38 @@ let id = Symbol('123');
 let anotherId = Symbol('123');
 console.log(id === anotherId); // false
  */
+
+
+// Stack and Heap type of memory 
+// Primitive data types uses stack type and non primitive uses heap memory
+// in stack type the copy of the value is provided while in the heap the the reference of the actual value is provided.
+
+let name1 = 'Manish';
+let name2 = name1;
+name2 = 'Bishnoi';
+console.log(name1); // name1 contaions the original value which is not chnaged when the name 2 assigned a new value because the copy was provided
+console.log(name2); 
+
+// in case of non primitive and heap data stack
+/*
+let user1 = {
+    name: 'Manish',
+    age: 22,
+    mail: 'mbishnoi@gmail.com'
+}
+let user2 = user1;
+// console.log(user2);
+
+user2.name = 'Bishnoi';
+console.log(user1);
+console.log(user2);
+
+let arr1 = ['hlw', 1,2, 'world'];
+let arr2 = arr1;
+arr2[1] = 'good';
+console.log(arr1);
+console.log(arr2);
+
+//in both the examples we can see that the reference of the original value was provided to the other variable so when we make changes in one the changes are made on the original oject or array.
+
+*/
