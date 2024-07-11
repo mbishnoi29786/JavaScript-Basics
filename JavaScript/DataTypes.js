917fcc1647055c7d501a04bc3d901ffdba3dfd0a
mbishnoi29786 <122912118+mbishnoi29786@users.noreply.github.com>
2024-07-11 07:35:43 +0000

    DataTypes.js

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,12 @@
+"use strict" // treat all code in js acoording to newer version
+
+// alert( 3+3 )       // this will not work in while using node, it works in browser
+
+// Data Types -->
+// 2 types --> 1. Primitive   2. Non-Primitive
+// 1. Primitive --> there are 7 premitive data type : 1.Number 2.BigInt 3.String 4.Boolean 5.Null 6.Undefined 7.Symbol
+// 2. Non-Primitive --> 1.Object
+
+// Null --> The result of typeof null is "object". That’s an officially recognized error in typeof, coming from very early days of JavaScript and kept for compatibility. Definitely, null is not an object. It is a special value with a separate type of its own. The behavior of typeof is wrong here.
+
+// Symbol --> for uniqueness
