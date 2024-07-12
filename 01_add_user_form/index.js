f2a6de6afb32326646abf74e78c0f2b5caa3f249
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-07-12 21:03:55 +0530

    Completed Adding an user and showing it in Table

--- 01_add_user_form/index.js
+++ 01_add_user_form/index.js
@@ -3,6 +3,7 @@ let existingUsers = [ ] ;
 document.getElementById('submitButton').addEventListener('click', addUser)
 
 function addUser(event) {
+
     event.preventDefault();
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
@@ -13,12 +14,36 @@ function addUser(event) {
         email: email,
         phone: phone
     }
-    console.log(newUser);
-    // Check if existingUsers is an array, if not reinitialize it
-    if (!Array.isArray(existingUsers)) {
-        existingUsers = [];
-    }
+    // console.log(newUser);
 
     existingUsers = [...existingUsers, newUser]
+    updateExistingUser(existingUsers)
     console.log(existingUsers);
+}
+
+
+function updateExistingUser(users){
+    let tableBody = document.getElementById('allUsersTableBody');
+    let allUsers = users;
+    console.log(allUsers.length);
+      // Clear existing rows if any
+        tableBody.innerHTML = '';
+    for(let i =0 ; i<allUsers.length; i++){
+        let newRow = tableBody.insertRow();
+        let newCell1 = newRow.insertCell(0);
+        let newCell2 = newRow.insertCell(1);
+        let newCell3 = newRow.insertCell(2);
+        newCell1.textContent = allUsers[i].name;
+        newCell2.textContent = allUsers[i].email;
+        newCell3.textContent = allUsers[i].phone;
+    }
+}
+
+// to search users
+
+document.getElementById('seachAllCurrentUsers').addEventListener('input', searhUsers);
+
+function searhUsers(){
+    let searchKeyword = document.getElementById('seachAllCurrentUsers').value;
+    
 }
