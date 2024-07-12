7b3282f70af591db5485a7a9bda7fe303d9c1a45
mbishnoi29786 <122912118+mbishnoi29786@users.noreply.github.com>
2024-07-12 13:30:32 +0000

    MInor Chnages in Add Form

--- Add_User_Form/index.js
+++ Add_User_Form/index.js
@@ -1,9 +1,9 @@
-let existingUsers = [] ;
+let existingUsers = [ ] ;
 
 document.getElementById('submitButton').addEventListener('click', addUser)
 
-
-function addUser() {
+function addUser(event) {
+    event.preventDefault();
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let phone = document.getElementById('phone').value;
@@ -14,6 +14,11 @@ function addUser() {
         phone: phone
     }
     console.log(newUser);
+    // Check if existingUsers is an array, if not reinitialize it
+    if (!Array.isArray(existingUsers)) {
+        existingUsers = [];
+    }
+
     existingUsers = [...existingUsers, newUser]
     console.log(existingUsers);
 }
