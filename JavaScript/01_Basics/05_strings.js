f3757b45493195bb4b27f4641f1b873346e6fd8d
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-07-15 19:03:27 +0530

    minor corrections in strings 05 in basics

--- JavaScript/01_Basics/05_strings.js
+++ JavaScript/01_Basics/05_strings.js
@@ -14,18 +14,18 @@ const gameName = new String('Manish123');
 // console.log(gameName.toUpperCase()); // MANISH123
 // console.log(gameName);   // [String : 'Manish123]  --> no Value change after toUpperCase as string comes in primitive data tyoes which follows stack memory means it gives only a copy to the variable.
 // console.log(gameName.charAt(3)); // i at 3rd index i is there  -->  we give index here on which we want to know the character.
-// we cannot give negative values in this we always get nothing.
-// when we provide a string in this it always gives the number at index position 
-// in case of booleans for true it gives 1 position and false 0
+    // we cannot give negative values in this we always get nothing.
+    // when we provide a string in this it always gives the number at index position 
+    // in case of booleans for true it gives 1 position and false 0
 
 // console.log(gameName.indexOf('n')); // 2   --> if it found the index of the number or char provided it will tell the index number else it will show -1 for all the other number char not found
-// if  2 char are provided it will find the first char position and print in the console but the condition is both the number should be present and in sequence. if not then it will show -1.
-// it also follows caseSensitivity.
+    // if  2 char are provided it will find the first char position and print in the console but the condition is both the number should be present and in sequence. if not then it will show -1.
+    // it also follows caseSensitivity.
 
 // const newString = gameName.substring(5,1);  // anis
 // console.log(newString);   // it gives a substring and thing to take note here is it excludes the last last index provided eg in this the substring is only created till 3 index and the 4 one is excluded.
-// we cannot provide negative value to the string if we give it will just start from the index till the second number indicate.
-// if we provide the first index bigger than the second it will still takes the lower one as first and the higher one second and create the string in the way it was return i will not reverse it.
+    // we cannot provide negative value to the string if we give it will just start from the index till the second number indicate.
+    // if we provide the first index bigger than the second it will still takes the lower one as first and the higher one second and create the string in the way it was return i will not reverse it.
 
 // const anoString = gameName.slice(4, -2);  // sh1 
 // console.log(anoString);  // Negative values are allowed here. these value will start from the end. it will give blank answer when it does not find inbetween the given bound
