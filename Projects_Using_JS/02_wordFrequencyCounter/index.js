f8ec2fe6c76d77f44525d2fde0f4f1366b75361c
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-07-15 19:02:15 +0530

    wordCount charaCount wordsInPara and wordFrequency done

--- Projects_Using_JS/02_wordFrequencyCounter/index.js
+++ Projects_Using_JS/02_wordFrequencyCounter/index.js
@@ -1,3 +1,54 @@
+// let inputPara = document.getElementById('inputPara').value;  // in node we cannot define document
+
+//Demo variable -->
+let demoInput = 'the quick brown fox jumps over the   lazy dog. dog dog  '
+
+// charaCount --> character count include spaces, line breaks etc
+let charaCount = demoInput.trim().length;
+// console.log(charaCount);
+// word extraction  --> extraction of all words from  
+let wordsInPara = demoInput.split(' ').filter(Boolean);   // using boolean will remove all the falsy value
+// let wordsInPara1 = demoInput.split(/\s+/);  // this one is using regular expression. here s represent character class space and + represent one or more
+// console.log(wordsInPara);
+
+// word Count --> total number of words in the para
+let wordCount = wordsInPara.length;
+// console.log(wordCount);
+
+
+// word frequency -- how many times a word has come in the para
+let wordFrequency = {};
+for(let i = 0; i<wordCount; i++){
+    let count = 1;
+    for (let k = i+1; k < wordCount; k++) {
+        if(wordsInPara[i] === null){
+            continue;
+        }
+        else if(wordsInPara[i]===wordsInPara[k]){
+            count++;
+            wordsInPara[k] = null;
+        }
+    }
+    if(wordsInPara[i] !== null){
+        wordFrequency[wordsInPara[i]] = count;
+    }
+}
+// console.log(wordFrequency);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 // Function to calculate word count
 function countWords(text) {
     // Clean up text and split into words
@@ -31,3 +82,4 @@ let inputText = "The Quick brown fox jumps over the lazy dog.";
 console.log("Word count:", countWords(inputText));
 console.log("Character count:", countCharacters(inputText));
 console.log("Word frequencies:", calculateWordFrequencies(inputText));
+*/
