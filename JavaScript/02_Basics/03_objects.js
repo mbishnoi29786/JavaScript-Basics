c07a3240d6ef5e71b9ca5e22308086fc0dc108e5
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-07-26 19:02:02 +0530

    objects injs

---
+++ JavaScript/02_Basics/03_objects.js
@@ -0,0 +1,63 @@
+// singleton --> when we create using constructor only one instacnce of object is created.
+// Object.create --> when we create array using this method it creates a singleton object.
+
+// if we create it using any other method it creates multiple instances of an object
+let jsUser = 
+{
+    myName : "Manish",
+    age : 24,
+    email: "mbishnoi29786@gmail.com",
+    lastLoginDays: ["Monday", "Tuesday"]
+}
+
+// one way to access the object keys
+// console.log(jsUser.lastLoginDays);  // --> Usage: Typically used when the property name is known at development time and is a valid JavaScript identifier.
+
+// other Way to access 
+// console.log(jsUser["email"]); // --> Useful when the property name is dynamic, stored in a variable, or when it's not a valid JavaScript identifier (e.g., contains spaces or special characters).
+
+// example --> using first one (using . notation) --> it does not support the symbol accessing
+// Define an object with static property names
+/*
+let person = {
+    name: 'manish',
+    age: 24,
+    email: 'manish@example.com'
+};
+
+// Access properties using dot notation
+console.log(person.name);   // Output: manish
+console.log(person.age);    // Output: 24
+console.log(person.email);  // Output: manish@example.com
+*/
+
+
+// example --> using second one (using []) --> it supports the symbol accessing
+// Define an object with dynamic and non-standard property names
+/*
+let user = {
+    'first name': 'manish',
+    'last name': 'bishnoi',
+    'email-address': 'manishbishnoi@example.com'
+};
+
+// Access properties using bracket notation
+let propertyName = 'email-address';
+console.log(user[propertyName]);  // Output: manishbishnoi@example.com
+
+// Another example with a dynamic property name
+let key = 'first name';
+console.log(user[key]);  // Output: manish
+*/
+
+// accessing a symbol in a object -->
+let sym = Symbol('description');
+let myObj =
+{
+    name: "manish",
+    "last name": "bishnoi",
+    email: "mbishnoi29786@gmail.com",
+    [sym]: 'value'
+}
+
+console.log(typeof myObj[mySym]);
