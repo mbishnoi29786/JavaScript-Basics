72d2147238e642b62dc92296fe2dfec185d513a3
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-07-27 22:37:32 +0530

    a little bit about destructuring an object and json

---
+++ JavaScript/02_Basics/05_objects.js
@@ -0,0 +1,39 @@
+// object destructuring -->
+
+let course = {
+    courseName : "abc",
+    price: 999,
+    courseInstructor: "hello"
+}
+
+let {courseName : abc} = course;
+
+console.log(abc);
+
+// in react when we do destructuring like this -->
+
+// let navBar = ({company}) => {
+
+// }
+
+// navBar(company = "abc")
+
+// when we retrieve data from database it gives us data in json format which means it sometimes give as an unnamed object in which both the key and values are in string format except numbers and boolean 
+// for example -->
+// {
+//     "name" : "abc",
+//     "lastname": "bbc"
+// }
+
+// it also sometimes gives an array in which it contains objects 
+// for example -->
+
+// [
+//     {
+
+//     },
+//     {
+
+//     }
+// ]
+
