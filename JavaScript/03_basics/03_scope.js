410bbbc53f40d2a069edbccd107c01af7e808240
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-07-29 23:40:51 +0530

    about call stack hoisting and and function call in different declaration methods

--- JavaScript/03_basics/03_scope.js
+++ JavaScript/03_basics/03_scope.js
@@ -30,3 +30,32 @@ console.log(k); // this will print the value if k
 console.log(l); // this will print the value of l
 
 // the problem of global variable is that if we use a same variable inside a scope and assign a value the previous value will be overridden so everytime we need to specify new variable which becomes troublesome in cases where a new programmer comes and edit the code and doesnot not that the variable has been declared already and change the value this can cause unexpected behaviour in the code.
+
+function abc()
+{
+    let a = "abc";
+
+    function bbc()
+    {
+        let b = "bbc";
+        console.log(a+b);
+    }
+    console.log(a+b);  // b cannot be accessed here. the child can inherit the varibales or properties from parent but the vice versa is not true
+}
+
+// it's same in the case of if else statements
+
+// function declaration 2 methods --> there is one more arrow function i will learn about that afterwards
+
+hlw(); // this will run without any problem as functions and variables are put first in the call stack
+
+function hlw ()
+{
+    console.log("Hello, World!");
+}
+
+let hlw2 = function (){
+    console.log("hello world 2");
+}
+
+hlw();  // this can only be called afterwards. even tough variable are put in call stack first but their values are assigned when the line of code come till then their value is stored as undefined.
