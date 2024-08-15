6ec7d62929c202d9e3af77d29461477e3d185860
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-08-15 17:07:49 +0530

    New KeyWord Explanation

--- JavaScript/10_classes_and_oops/objects_prototype.js
+++ JavaScript/10_classes_and_oops/objects_prototype.js
@@ -160,3 +160,23 @@ book2.displayInfo(); // Output: Title: To Kill a Mockingbird, Author: Harper Lee
 // By adding methods to Book.prototype, we ensure that all books can perform the same actions. For example, the displayInfo method is available to every book object, showing its title and author.
 // Creating and Using Books
 // When we create book1 and book2 using new, we get two separate book objects with their own data. Both share the displayInfo method, so they can both display their information.
+
+
+// some extra information 
+
+/*
+
+oop.js
+
+Here's what happens behind the scenes when the new keyword is used:
+
+A new object is created: The new keyword initiates the creation of a new JavaScript object.
+
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructor's prototype.
+
+The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object,
+to be the intended return value.
+
+The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
+
+*/
