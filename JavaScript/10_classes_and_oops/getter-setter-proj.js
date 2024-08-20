5c542eb47c114033831be469dea890388c121e50
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-08-20 13:26:10 +0530

    getter-setter-proj some miner details added

--- JavaScript/10_classes_and_oops/getter-setter-proj.js
+++ JavaScript/10_classes_and_oops/getter-setter-proj.js
@@ -8,8 +8,8 @@ class SecureUser
 {
     constructor(username, password)
     {
-        this._username = username;
-        this.password = password; // Store raw password internally
+        this._username = username; // Internal storage for username
+        this.password = password; // Calls setter to hash and store password
     }
 
     // Getter for username
@@ -27,7 +27,7 @@ class SecureUser
     // custom getter for password (return masked password for security)
     get password()
     {
-        return '*******';  // Masked Password
+        return '********'  // Masked Password
     }
     
     // custom setter for password (hashes password before string)
@@ -57,3 +57,11 @@ console.log(`Password: ${secureUser.password}`); // Password: ********
 // Authenticate user
 console.log(secureUser.authenticateUser('Bishnoi')); // true
 console.log(secureUser.authenticateUser('hello,World')); // false
+
+
+// Main thing to care in this project or the problem i faced infinite recursion.
+// Infinite recursion: 
+// when working with getters and setters, using the same name for a property and its accessor methods (getter and setter) can lead to issues like infinite recursion. This is why distinguishing between the property name and its backing variable is important.
+
+// Understanding Getters and Setters
+// Getters and setters are special methods in JavaScript that allows us to define custom behavior for reading and writing properties. They are used to create computed properties or to control the access and modification of object properties.
